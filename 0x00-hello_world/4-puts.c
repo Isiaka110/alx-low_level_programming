7972542196bed8c02e37e4bd132df3ea7cb8e076
isiaka110 <oshio.choco1@gmail.com>
2023-08-10 13:51:21 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main Program that prints a line with put function
+ * Return : 0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
