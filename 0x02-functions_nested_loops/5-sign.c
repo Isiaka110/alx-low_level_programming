006d70e432a7d8ad15295e753501d26b73fb1a94
isiaka110 <oshio.choco1@gmail.com>
2023-08-15 11:35:59 -0700

    check if corect

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,26 @@
-#include "main.h"
-/**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
- */
-int print_sign(int n)
-{
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	} else if (n == 0)
+#include <stdio.h>
+
+int print_sign(int n) {
+    if (n > 0) {
+        printf("+");
+        return 1;
+    } else if (n < 0) {
+        printf("-");
+        return -1;
+    } else {
+        printf("0");
+        return 0;
+    }
 }
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
-	}
-		return (-1);
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    int result = print_sign(number);
+    printf("\nSign: %d\n", result);
+
+    return 0;
 }
 
