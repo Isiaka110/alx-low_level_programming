37737d39e5681446048013ac46814ee040967959
isiaka110 <oshio.choco1@gmail.com>
2023-09-05 06:08:04 -0700

    Correct this time

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,17 +2,15 @@
 #include "main.h"
 
 /**
- * main - print the number of arguments passed to the program
+ * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
-
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-(void) argv; /*Ignore argv*/
-	printf("%d\n", argc - 1);
+	printf("%s\n", *argv);
 
 	return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
-
 #include "main.h"
 
-
 /**
-
- * main - print the number of arguments passed to the program
-
+ * main - prints all arguments it receives
  * @argc: number of arguments
-
  * @argv: array of arguments
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
-
 int main(int argc, char *argv[])
-
 {
+	int i;
 
-(void) argv; /*Ignore argv*/
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 
-        printf("%d\n", argc - 1);
-
-
-        return (0);
-
+	return (0);
 }
+
